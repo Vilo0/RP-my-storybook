b8807a5c891c801f97f59f793e543b1f08a0a617
Edgard Vilo <evilo@hostyweb.cl>
2022-02-19 10:30:42 -0300

    Clase 162: Tarea - Propiedades y controles adicionales

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -9,15 +9,27 @@ export interface Props {
     * Este es el tamaño por defecto del label
     */
     size: 'normal' | 'h1'| 'h2'| 'h3';
+
+    /**
+    * Este es el color que tendrá el label
+    */
+    color: 'primary' | 'secondary' | 'tertiary';
+
+    /**
+    * Este es la opción de capizalizar el texto
+    */
+    allCaps: boolean;
 }
 
 export const MyLabel = ({ 
     label = 'No Label', 
-    size = 'normal'
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
 }: Props) => {
   return (
-    <span className={ `${ size }` }>
-       { label }
+    <span className={ `${ size } text-${color}` }>
+       { allCaps ? label.toUpperCase() : label }
     </span>
   )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -4,6 +4,10 @@ import { MyLabel } from '../../components/MyLabel';
 export default {
     title: 'UI/MyLabel',
     component: MyLabel,
+    argTypes: {
+        size: { control: 'select' }, // por defecto agrega todas las opciones, pero puedes usar el options en el caso de querer seleccionar solo algunas opciones
+        color: { control: 'select' },
+    }
 } as ComponentMeta<typeof MyLabel>;
 
 const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel { ...args } />
@@ -12,9 +16,26 @@ const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel { ...args } 
 export const Basic = Template.bind({});
 Basic.args = {
     label: 'Basic Label',
-    size: 'h3',
+    size: 'normal',
+    allCaps: false // true: capitalizar toda la palabra
 };
 
 export const AllCaps = Template.bind({});
+AllCaps.args = {
+    size: 'normal',
+    allCaps: true,
+}
 
 export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'normal',
+    color: 'secondary', // primary | secondary | tertiary
+    allCaps: false,
+}
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+    size: 'normal',
+    color: 'tertiary',
+    allCaps: false,
+}
